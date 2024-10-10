0ffedcefd5ac70e797132ed3448e08bbd92e45f2
BeatrizBalaguer <B.Balaguer@sms.ed.ac.uk>
2024-10-10 16:50:38 +0100

    Initial commit

---
+++ Week 4 Prep/Packages.R
@@ -0,0 +1,14 @@
+markdown_practice <- c(   
+  "rmarkdown",   
+  "tinytex",   
+  "tidyverse",   
+  "knitr",   
+  "remotes",   
+  "gapminder",   
+  "shiny",   
+  "flexdashboard",   
+  "here")
+install.packages(markdown_practice)
+
+tinytex::install_tinytex()
+tinytex:::is_tinytex()
